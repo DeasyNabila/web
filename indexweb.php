a86076c9958894f69a91f688949e5d85b4ae50af
DeasyNabila <deasy_nabila_29rpl@student.smktelkom-mlg.sch.id>
2021-12-06 20:02:49 +0700

    WEB

---
+++ indexweb.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                Covid-19
+            </div>
+        </header>
+        <nav>
+             <ul>
+                 <li><a href="index.html">Beranda</a></li>
+                 <li><a href="kontak-saya.html">Kontak Saya</a></li>
+                 <li><a href="tentang-saya.html">Tentang Saya</a></li>
+             </ul>
+         </nav>
+        <article>
+            <div class="konten">
+                <a class = "covid" href="#"> <img  src="covid.jpg" alt="" ></a>
+             <div class="judul">
+                 <a href="covid.jpg">Covid</a>
+             </div>
+            <p>Coronavirus atau disebut juga dengan virus corona merupakan keluarga besar virus yang mengakibatkan terjadinya infeksi saluran pernapasan atas ringan hingga sedang, seperti penyakit flu. Banyak orang terinfeksi virus ini, setidaknya satu kali dalam hidupnya.</p>
+            </div>
+            <div class="konten">
+                <a class = "gejala" href="#"> <img  src="gejala.jpg" alt="" ></a>
+             <div class="judul">
+                 <a href="gejala.html">Gejala</a>
+             </div>
+            <p>Secara umum, ada tiga gejala umum yang bisa menandakan seseorang terinfeksi virus Corona, yaitu:
+
+                1. Demam (suhu tubuh di atas 38 derajat Celsius)
+                2. Batuk kering
+                3. Sesak napas </p>
+            </div>
+            <div class="konten">
+                <a class = "penyebab" href="#"> <img  src="penyebab.jpg" alt="" ></a>
+             <div class="judul">
+                 <a href="penyebab.html">Penyebab</a>
+             </div>
+             <p>Infeksi coronavirus disebabkan oleh virus corona itu sendiri. Kebanyakan virus corona menyebar seperti virus lain pada umumnya, melalui: 
+
+                1. Percikan air liur pengidap (batuk dan bersin).
+                2. Menyentuh tangan atau wajah orang yang terinfeksi.
+                3. Menyentuh mata, hidung, atau mulut setelah memegang barang yang terkena percikan air liur pengidap virus corona. 
+                4. Tinja atau feses (jarang terjadi), </p>
+            </div>
+            <div class="konten">
+                <a class = "pengobatan" href="#"> <img  src="pengobatan.jpg" alt="" ></a>
+             <div class="judul">
+                 <a href="pengobatan.html">Pengobatan</a>
+             </div>
+            <p>Pada pasien bergejala ringan atau tanpa gejala, dokter akan menyarankan untuk isolasi mandiri di rumah, sambil tetap melakukan langkah-langkah pencegahan penyebaran infeksi virus Corona. Beberapa tindakan yang dapat dilakukan oleh dokter adalah:
+
+                1. Merujuk pasien dengan gejala berat untuk menjalani perawatan dan karantina di rumah sakit rujukan
+                2. Meresepkan obat pereda demam dan nyeri yang aman dan sesuai dengan kondisi pasien
+                3. Menganjurkan pasien untuk melakukan isolasi mandiri dan istirahat yang cukup
+                4. Menganjurkan pasien untuk banyak minum air putih untuk menjaga kadar cairan tubuh</p>
+            </div>
+       </article>
+   
+        <footer>
+             Copyright 2020 corona-virus.com
+        </footer>
+     </div>
+ </body>
+ </html>
+</body>
+</html>
